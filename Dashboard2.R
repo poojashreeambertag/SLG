92c786171770a618f09eb4096cd60f865e247cbf
poojashreeambertag <44161865+poojashreeambertag@users.noreply.github.com>
2018-10-31 12:24:45 +0530

    Add files via upload

---
+++ Dashboard2.R
@@ -0,0 +1,115 @@
+setwd('/home/poojashree/Downloads/')
+library(shiny)
+require(shinydashboard)
+library(ggplot2)
+library(dplyr)
+recommendation <- read.csv('Prediction.csv',stringsAsFactors = F,header=T)
+#head(recommendation)
+#Dashboard header carrying the title of the dashboard
+header <- dashboardHeader(title = "Basic Dashboard")  
+#Sidebar content of the dashboard
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Visit-us", icon = icon("send",lib='glyphicon'), 
+             href = "https://www.salesforce.com")
+  )
+)
+frow1 <- fluidRow(
+  valueBoxOutput("value1")
+  ,valueBoxOutput("value2")
+  ,valueBoxOutput("value3")
+)
+frow2 <- fluidRow(
+  
+  box(
+    title = "Forecasted_Generation.in.MW"
+    ,status = "primary"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,plotOutput("revenuebyPrd", height = "300px")
+  )
+  
+  ,box(
+    title = "Available_Capacity.AvC.MW"
+    ,status = "primary"
+    ,solidHeader = TRUE 
+    ,collapsible = TRUE 
+    ,plotOutput("revenuebyRegion", height = "300px")
+  ) 
+  
+)
+# combine the two fluid rows to make the body
+body <- dashboardBody(frow1, frow2)
+#completing the ui part with dashboardPage
+ui <- dashboardPage(title = 'This is my Page title', header, sidebar, body, skin='red')
+# create the server functions for the dashboard  
+server <- function(input, output) { 
+  
+  #some data manipulation to derive the values of KPI boxes
+  #total.revenue <- sum(recommendation$Revenue)
+  #sales.account <- recommendation %>% group_by(Account) %>% summarise(value = sum(Revenue)) %>% filter(value==max(value))
+  #prof.prod <- recommendation %>% group_by(Product) %>% summarise(value = sum(Revenue)) %>% filter(value==max(value))
+  
+  
+  #creating the valueBoxOutput content
+  recommendation$Forecasted_Generation.in.MW <- renderValueBox({
+    valueBox(
+      #formatC(sales.account$value, format="d", big.mark=',')
+      #,paste('Top Account:',sales.account$Account)
+      ,icon = icon("stats",lib='glyphicon')
+      ,color = "purple")
+    
+    
+  })
+  
+}
+shinyApp(ui, server)   
+  
+  output$value2 <- renderValueBox({
+    
+    valueBox(
+      formatC(total.revenue, format="d", big.mark=',')
+      ,'Total Expected Revenue'
+      ,icon = icon("gbp",lib='glyphicon')
+      ,color = "green")
+    
+  })
+  
+  
+  
+  output$value3 <- renderValueBox({
+    
+    valueBox(
+      formatC(prof.prod$value, format="d", big.mark=',')
+      ,paste('Top Product:',prof.prod$Product)
+      ,icon = icon("menu-hamburger",lib='glyphicon')
+      ,color = "yellow")
+    
+  })
+  
+  #creating the plotOutput content
+  
+  output$revenuebyPrd <- renderPlot({
+    ggplot(data = recommendation, 
+           aes(x=Product, y=Revenue, fill=factor(Region))) + 
+      geom_bar(position = "dodge", stat = "identity") + ylab("Revenue (in Euros)") + 
+      xlab("Product") + theme(legend.position="bottom" 
+                              ,plot.title = element_text(size=15, face="bold")) + 
+      ggtitle("Revenue by Product") + labs(fill = "Region")
+  })
+  
+  
+  output$revenuebyRegion <- renderPlot({
+    ggplot(data = recommendation, 
+           aes(x=Account, y=Revenue, fill=factor(Region))) + 
+      geom_bar(position = "dodge", stat = "identity") + ylab("Revenue (in Euros)") + 
+      xlab("Account") + theme(legend.position="bottom" 
+                              ,plot.title = element_text(size=15, face="bold")) + 
+      ggtitle("Revenue by Region") + labs(fill = "Region")
+  })
+  
+  
+  
+}
+shinyApp(ui, server) 
